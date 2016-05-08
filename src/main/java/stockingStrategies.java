c84ff7d2b7045ec901d157d25cf095789b2a9a46
Michał Czarnecki <mich.czarnecki@o2.pl>
2016-05-08 22:18:44 +0200

    Sending request to application

--- src/main/java/stockingStrategies.java
+++ src/main/java/stockingStrategies.java
@@ -1,5 +1,17 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+import java.net.URL;
+
 public class stockingStrategies {
-    public static void main(String... args){
-        System.out.println("Michał tu był");
+    public static void main(String... args) throws IOException {
+
+        String url = "http://localhost:5001/calculateDailyStrategies";
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setReadTimeout(15*1000);
+        int responseCode = con.getResponseCode();
     }
 }
